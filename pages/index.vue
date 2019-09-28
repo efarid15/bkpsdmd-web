ac8d7a8f797a38fb45867668351192e7c57755c3
Gustang <gustangkd@gmail.com>
2019-09-28 13:54:22 +0800

    first commit branch dev

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <a-row type="flex" justify="space-around" align="middle" :style="{ height: '100vh',padding: '24px' }">
     <a-col :xs="24" :sm="12" :md="12" :lg="6">
       <a-form
-        id="components-form-demo-normal-login"
         :form="form"
         class="login-form"
         @submit="handleSubmit"
