49c6c4d9276dc53561edb37c6d09c8bb604297af
Gustang <gustangkd@gmail.com>
2019-10-03 23:51:21 +0800

    add homepages slide full

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div style="width: 100%;height: 100vh">
-    <div class="d-flex align-items-center" style="height: 100%">
-      <div style="margin: 0 auto">
-        <a-button type="primary" size="large">
-          <nuxt-link to="/login">Login</nuxt-link>
-        </a-button>
+  <a-layout class="ant-layout--onepages">
+    <a-layout-content>
+      <div class="ant-carousel--slide">
+        <a-carousel vertical autoplay effect="fade">
+          <div
+            class="ant-carousel--slide-images"
+            style="backgroundImage: url('https://images.unsplash.com/photo-1568284440614-7155befdc39d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2855&q=80')"
+          ></div>
+          <div
+            class="ant-carousel--slide-images"
+            style="backgroundImage: url('https://images.unsplash.com/photo-1497091071254-cc9b2ba7c48a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2853&q=80')"
+          ></div>
+        </a-carousel>
+        <div class="ant-carousel--slide-caption">
+          <div class="title">SIPP BPSM</div>
+          <div class="subtitle">Badan Kepegawaian Dan Sumber Daya Manusia Kota Makassar</div>
+          <a-button size="large">
+            <nuxt-link to="/login">Masuk ke Dashboard <a-icon type="right" /></nuxt-link>
+          </a-button>
+        </div>
       </div>
-    </div>
-  </div>
+    </a-layout-content>
+  </a-layout>
 </template>
 <script>
 export default {
